51ca6bb2edccd577e2831b2a90c88ec51f4ed546
mrjadaml <mrjadaml@gmail.com>
2015-02-27 12:03:37 -0700

    edit mode is now toggleable

--- app/controllers/rants.js
+++ app/controllers/rants.js
@@ -1,5 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+  editTemp: false,
+
+  actions: {
+    editRant: function(rant) {
+      rant.set('editTemp', true);
+    },
+
+    deleteRant: function () {
+
+    }
+  },
 
 });
---
+++ app/things.md
@@ -0,0 +1,5 @@
+the side of my atom editor.
+
+my file structure on my comp
+
+technology tree -> Civilization games
