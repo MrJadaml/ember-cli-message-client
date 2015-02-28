808d7a9cd3aa848b033526ce9ca859d8c10d174d
mrjadaml <mrjadaml@gmail.com>
2015-02-28 15:29:44 -0700

    edit rant sets title and body for edit mode. Cleared rant title and body for save

--- app/controllers/rants.js
+++ app/controllers/rants.js
@@ -6,10 +6,26 @@ export default Ember.ArrayController.extend({
   actions: {
     editRant: function(rant) {
       rant.set('editTemp', true);
+      this.set('rantTitle', rant.get('title'));
+      this.set('rantBody', rant.get('body'));
+    },
+
+    saveRant: function (rant) {
+      var title = this.get('rantTitle');
+      var body = this.get('rantBody');
+      if (body) {
+        rant.set('title', title);
+        rant.set('body', body);
+      }
+      rant.save().then(function() {
+        this.set('rantTitle', '');
+        this.set('rantBody', '');
+        rant.set('editTemp', false);
+      }.bind(this));
     },
 
     cancelRant: function (rant) {
-      rant.set('editTemp', false)
+      rant.set('editTemp', false);
     },
 
     deleteRant: function () {
