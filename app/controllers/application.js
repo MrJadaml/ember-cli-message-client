2558f52624c9dab20ead174b23e12fe264dba8f5
mrjadaml <mrjadaml@gmail.com>
2015-03-25 08:02:31 -0600

    created init fn in application to persist isAuthenticated

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,6 +3,14 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   needs: ['search'],
   isAuthenticated: false,
+  
+  init: function() {
+    var authToken = localStorage.getItem('authToken');
+    if(authToken) {
+      this.isAuthenticated = true;
+    }
+    // http.post('/authenticate').then() unless isAuthenticated
+  },
 
   actions: {
     doSearch: function () {
