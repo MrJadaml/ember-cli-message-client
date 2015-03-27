cea80aa78e61a2d7d3d810533926d50691e59194
mrjadaml <mrjadaml@gmail.com>
2015-03-27 09:36:11 -0600

    added user model, and user association with rants model

--- app/models/rant.js
+++ app/models/rant.js
@@ -3,6 +3,9 @@ import DS from 'ember-data';
 var Rant = DS.Model.extend({
   title: DS.attr('string'),
   body: DS.attr('string'),
+  user: DS.belongsTo('user'),
+  createdAt: DS.attr('date'),
+  updatedAt: DS.attr('date'),
 });
 
 Rant.reopenClass({
